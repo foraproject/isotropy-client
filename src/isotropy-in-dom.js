8363a4525b8122a6d36d499c2a0e627aae974754
Jeswin Kumar <jeswinpk@agilehead.com>
2016-03-05 11:53:47 +0530

    Switch to shared flow interfaces

--- src/isotropy-in-dom.js
+++ src/isotropy-in-dom.js
@@ -6,9 +6,9 @@ import Router from "isotropy-router";
 import getPage from "isotropy-page";
 import type { PluginType } from "isotropy-core";
 import type { IsotropyOptionsType, IsotropyResultType } from "isotropy-core";
-import type { IncomingMessage, ServerResponse, Server } from "./flow/http";
+import type { IncomingMessage, ServerResponse, Server } from "isotropy-interfaces/node/http-in-browser";
 
-type IsotropyFnType = (apps: Object, options: IsotropyOptionsType) => Promise<IsotropyResultType>;
+type IsotropyFnType = (apps: Object, options: IsotropyOptionsType<IncomingMessage, ServerResponse>) => Promise<IsotropyResultType>;
 
 export default async function(apps: Object, plugins: Array<PluginType>, options: IsotropyOptionsType) : Promise<IsotropyResultType> {
   const page = getPage();
@@ -22,14 +22,18 @@ export default async function(apps: Object, plugins: Array<PluginType>, options:
   const result = await isotropy(apps, options);
   const server = result.server;
 
-  page('*', () => {
-    const req = new http.IncomingMessage();
-    req.method = "GET";
-    req.url = window.location.href;
-    const res = new http.ServerResponse();
-    server.requestListener(req, res);
-  });
-  page();
-  server.page = page;
+  if (server) {
+    page('*', () => {
+      const req = new http.IncomingMessage();
+      req.method = "GET";
+      req.url = window.location.href;
+      const res = new http.ServerResponse();
+      server.requestListener(req, res);
+    });
+    page();
+    server.page = page;
+  } else {
+    throw new Error("isotropy did not return a server.");
+  }
   return result;
 };
